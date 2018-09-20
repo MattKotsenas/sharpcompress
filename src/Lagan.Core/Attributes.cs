d8b2918443be7e19120fdea81c61cc926dc89468
Matt Kotsenas <matt.kotsenas@gmail.com>
2018-09-20 15:25:15 -0700

    add Lagan.Core and Lagan.Analyzers skeletons

---
+++ src/Lagan.Core/Attributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lagan.Core
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false, Inherited = true)]
+    public class OwnedAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false, Inherited = true)]
+    public class BorrowedAttribute : Attribute
+    {
+    }
+}
